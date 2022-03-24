d1e5efb256f746266b0a0636e6395533eda945c6
siyan <zhangsiyan12134@gmail.com>
2022-03-24 11:41:25 -0400

    Added va_list test

---
+++ va_list_test.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+
+int ctfs_fcntl(int fd, int cmd, ...)
+{
+    va_list ap;
+    va_start(ap, cmd); 
+    printf("Normal Paras: %d %d\n", fd, cmd);
+    printf("Received: %d\n", va_arg(ap, int));  // retuens a parameter for each call
+    printf("Received: %d\n", va_arg(ap, int));
+    printf("Received: %d\n", va_arg(ap, int));
+    printf("Received: %d\n", va_arg(ap, int));
+    va_end(ap);
+    return 0;
+}
+
+int main(int argc, char **argv)
+{
+    ctfs_fcntl(10, 1, 2, 3, 4, 5);
+    return 0;
+}
